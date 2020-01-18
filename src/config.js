415d5d2faf6e25c8f1aa8a56b55fdc5a932cb369
Wenye Lu <luwenye2010@gmail.com>
2020-01-18 22:52:44 +0800

    deployed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default{
-    API_FOLDER_ENDPOINT: `https://protected-plateau-29148.herokuapp.com/${folder}`,
-    API_NOTE_ENDPOINT: `https://protected-plateau-29148.herokuapp.com/${note}`
+    API_FOLDER_ENDPOINT: `https://protected-plateau-29148.herokuapp.com/folder`,
+    API_NOTE_ENDPOINT: `https://protected-plateau-29148.herokuapp.com/note`
 }
